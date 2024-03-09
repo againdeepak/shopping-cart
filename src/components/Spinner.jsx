66b7d4368b1ffe89a3d1402c97d993c08b1a4985
Deepak <deepakkumar09092003@gmail.com>
2024-03-10 04:34:42 +0530

    Shopping Cart

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div>
+      <div className='spinner loader'></div>
+      <p className='loader'>Loading...</p>
+    </div>
+  )
+}
+
+export default Spinner
